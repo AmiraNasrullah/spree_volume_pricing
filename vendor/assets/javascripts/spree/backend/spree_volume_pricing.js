2d89b23d2b7197075867ad759d4429bd3dacd8b1
AmiraNasrullah <amira.nasrallah92@gmail.com>
2015-06-17 11:43:25 +0200

    [MOD] add volume price by javascript

--- vendor/assets/javascripts/spree/backend/spree_volume_pricing.js
+++ vendor/assets/javascripts/spree/backend/spree_volume_pricing.js
@@ -1,23 +1,34 @@
 // spree's version only handles 'input', not 'select', and this breaks spree_volume_pricing
+//= require spree/backend
 
 $(function () {
-  $('.new_volume_price_row.add_fields').off('click').on('click', function() {
+  var uniqueId = 1;
+  $('.new_volume_price_row.spree_add_fields').off('click').on('click', function() {
     var target = $(this).data("target");
     var new_table_row = $(target + ' tr:visible:last').clone();
-    var new_id = new Date().getTime();
-    new_table_row.find("input,select").each(function () {
+    var new_id = new Date().getTime() + (uniqueId++);
+    new_table_row.find("input, select").each(function () {
       var el = $(this);
-      el.val("");
-      el.attr("id", el.attr("id").replace(/\d+/, new_id))
-      el.attr("name", el.attr("name").replace(/\d+/, new_id))
+      if (!el.is("select")){
+        el.val("");
+      }
+      el.prop("id", el.prop("id").replace(/\d+/, new_id))
+      el.prop("name", el.prop("name").replace(/\d+/, new_id))
+      el.prop("checked", false);
     });
+    new_table_row.find('.select2-container').remove();
+
+    var discount_type_select = new_table_row.find('.select2');
+    discount_type_select.removeClass('select2-offscreen');    
+    var selectOption = discount_type_select.find("option").first()
+    discount_type_select.select2({val: selectOption}).change();
     // When cloning a new row, set the href of all icons to be an empty "#"
     // This is so that clicking on them does not perform the actions for the
     // duplicated row
     new_table_row.find("a").each(function () {
       var el = $(this);
-      el.attr('href', '#');
-    });
+      el.prop('href', '#');
+    })
     $(target).prepend(new_table_row);
   });
 
